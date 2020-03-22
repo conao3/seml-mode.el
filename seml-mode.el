540d37d7da4e9968ea4db59c2333f284b5c28f48
conao3 <conao3@gmail.com>
2020-03-22 19:49:30 +0900

    fix sample config

--- seml-mode.el
+++ seml-mode.el
@@ -63,15 +63,13 @@
 
 ;; Sample configuration with [[https://github.com/conao3/leaf.el][leaf.el]]
 
-;; (leaf real-auto-save
-;;   :ensure t
-;;   :custom ((real-auto-save-interval . 0.3))
-;;   :hook (find-file-hook . real-auto-save-mode))
-
-;; (leaf seml-mode
-;;   :config (require 'seml-mode)
-;;   :custom ((seml-live-refresh-interval . 0.35)))
-
+;;   (leaf seml-mode
+;;     :custom ((seml-live-refresh-interval . 0.35))
+;;     :config
+;;     (leaf real-auto-save
+;;       :ensure t
+;;       :custom ((real-auto-save-interval . 0.3))
+;;       :hook (find-file-hook . real-auto-save-mode)))
 
 ;;; Code:
 
