bea83829dbfd151a93277085ef7ed70c51b62435
conao3 <conao3@gmail.com>
2020-03-22 19:47:50 +0900

    fix package commentaty

--- seml-mode.el
+++ seml-mode.el
@@ -23,14 +23,25 @@
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;;
+
 ;; Below 2 files represent the same structure.
 ;; I call the S expression representation of the markup language
 ;; (especially with HTML) SEML and this package provides
 ;; the major mode and utility for that file.
-;;
+
 ;; SEML is *short* and *easy to understand* for Lisp hacker.
-;;
+
+;; #+begin_src seml
+;;   (html ((lang . "en"))
+;;     (head nil
+;;       (meta ((charset . "utf-8")))
+;;       (title nil "sample page")
+;;       (link ((rel . "stylesheet") (href . "sample1.css"))))
+;;     (body nil
+;;       (h1 nil "sample")
+;;       (p nil "text sample")))
+;; #+end_src
+
 ;; #+begin_src html
 ;;   <!DOCTYPE html>
 ;;   <html lang="en">
@@ -47,31 +58,20 @@
 ;;     </body>
 ;;   </html>
 ;; #+end_src
-;;
-;; #+begin_src seml
-;;   (html ((lang . "en"))
-;;     (head nil
-;;       (meta ((charset . "utf-8")))
-;;       (title nil "sample page")
-;;       (link ((rel . "stylesheet") (href . "sample1.css"))))
-;;     (body nil
-;;       (h1 nil "sample")
-;;       (p nil "text sample")))
-;; #+end_src
-;;
+
 ;; More information at [[https://github.com/conao3/seml-mode.el][github]]
-;;
+
 ;; Sample configuration with [[https://github.com/conao3/leaf.el][leaf.el]]
-;;
+
 ;; (leaf real-auto-save
 ;;   :ensure t
 ;;   :custom ((real-auto-save-interval . 0.3))
 ;;   :hook (find-file-hook . real-auto-save-mode))
-;;
+
 ;; (leaf seml-mode
 ;;   :config (require 'seml-mode)
 ;;   :custom ((seml-live-refresh-interval . 0.35)))
-;;
+
 
 ;;; Code:
 
